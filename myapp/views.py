f6a46c345f5ee5663839fa411aa93acacd28fe2b
HongYuan <2630988386@qq.com>
2024-08-20 17:08:11 +0800

    v1

--- myapp/views.py
+++ myapp/views.py
@@ -1,3 +1,35 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+import praw
+from datetime import datetime
+from django.http import JsonResponse
+
+
+def get_posts(request):
+    reddit = praw.Reddit(
+        client_id="ufzJ6zOL3o8CPP2ebkVLOw",
+        client_secret="w6CfWbdMjFS2vLIDEbE3MdAeOz6pGQ",
+        user_agent="script:RedditDataMiner:1.0 (by /u/Natural-Loquat-4982)",
+        username="Natural-Loquat-4982",
+        password="Hzy20040901"
+    )
+
+    subreddit = reddit.subreddit('all')
+    queries = ["AI tools PPT", "AI tools deck", "AI tool for voice clone"]
+    time_filter = "week"
+    results = []
+
+    for query in queries:
+        search_results = subreddit.search(query, time_filter=time_filter, limit=10)
+        for submission in search_results:
+            post_info = {
+                "title": submission.title,
+                "posted_on": datetime.utcfromtimestamp(submission.created_utc).isoformat(),
+                "text": submission.selftext,
+                "score": submission.score,
+                "url": submission.url
+                }
+            results.append(post_info)
+    return JsonResponse({"results": results})
